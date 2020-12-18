a9d7a43eab558584f4c7b2b9f5cdd8d50ec46f04
J-TechTips <jtecktips@gmail.com>
2020-12-17 18:12:12 -0800

    structured-english complete

---
+++ IS51Final.py
@@ -0,0 +1,14 @@
+"""
+Program will display grades of students on the final by: 
+    -number of grades
+    -average grade
+    -percentage of grades above the average
+The average grades is 83.25.
+The number of grades is 24.
+The percentage of grades above average is 54.17%
+
+We will bring in the list in a dictionary by using the open method.
+The grades then will be listed as integers.
+Then the calculations can begin.
+End result is displaying the 3 solutions.
+"""
